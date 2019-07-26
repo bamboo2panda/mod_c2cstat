a7d0161301b49cf9f638644c32133daed5392850
Pavel <bamboo2panda@gmail.com>
2019-07-26 16:39:30 +0300

    first commit

---
+++ mod_mod_c2cstat.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package    mod_c2cstat
+ *
+ * @author     Pavel <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+defined('_JEXEC') or die;
+
+$moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
+
+require ModuleHelper::getLayoutPath('mod_mod_c2cstat', $params->get('layout', 'default'));
