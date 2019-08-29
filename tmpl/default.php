b855b167ed2bdcf9150bd03e6b17614225a2688a
Pavel <bamboo2panda@gmail.com>
2019-08-29 16:43:12 +0300

    =Frontend= - Pointer cursor. =Backend= - Icon field

--- tmpl/default.php
+++ tmpl/default.php
@@ -40,11 +40,11 @@ $document->addScript(JURI::root(). 'media/mod_c2cstat/script.js');
         console.log(msg,sel);
     }
     dataset = {
-        "children": [{"Name":"Московская электронная школа","Count":5,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Пермь</li><li>Город А</li><li>Город Б</li></ul>"},
-            {"Name":"Образовательное телевидение (МособрТВ)","Count":4,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Каспийск</li><li>Город А</li></ul>"},
+        "children": [{"Name":"Московская электронная школа","Count":3,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Пермь</li></ul>"},
+            {"Name":"Образовательное телевидение (МособрТВ)","Count":2,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Каспийск</li></ul>"},
             {"Name":"Менторинг в системе образования","Count":3,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Сочи</li></ul>"},
-            {"Name":"Субботы московского школьника","Count":2,"City":"Москва","Partners":"<ul><li>Тюмень</li></ul>"},
-            {"Name":"Обучение слепых детей в обычных классах","Count":1,"City":"Ижевск","Partners":""}]
+            {"Name":"Субботы московского школьника","Count":3,"City":"Москва","Partners":"<ul><li>Тюмень</li></ul>"},
+            {"Name":"Обучение слепых детей в обычных классах","Count":1,"City":"Ижевск","Partners":"<ul><li>Ижевск</li></ul>"}]
 
     };
 
@@ -78,9 +78,9 @@ $document->addScript(JURI::root(). 'media/mod_c2cstat/script.js');
         // .style("opacity", 0)
 
     }
-    var chartDiv = document.getElementById("my_dataviz");
+
     var bubble = d3.pack(dataset)
-        .size([chartDiv.clientWidth, chartDiv.clientHeight])
+        .size([diameter, diameter])
         .padding(1.5);
 
     var svg = d3.select("#my_dataviz")
@@ -112,10 +112,8 @@ $document->addScript(JURI::root(). 'media/mod_c2cstat/script.js');
         .style("fill", function(d,i) {
             return color(i);
         })
-        .attr("stroke", "#9d9d9d")
+        .attr("stroke", "white")
         .style("stroke-width", "2px")
-        .style("cursor", "pointer")
-        .on("click", mouseover)
     // .on("mouseover", mouseover)
     // .on("mousemove", mousemove)
     // .on("mouseleave", mouseleave)
