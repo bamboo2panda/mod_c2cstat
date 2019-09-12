6dd384ec19242b88925ff0d4c4044213f99d05bf
Pavel <bamboo2panda@gmail.com>
2019-09-12 13:13:00 +0300

    City filter.

--- tmpl/default.php
+++ tmpl/default.php
@@ -52,17 +52,15 @@ $s = array_unique($s);
         }
         return _p8() + _p8(true) + _p8(true) + _p8();
     }
-    function log(sel,msg) {
-        console.log(msg,sel);
-    }
-    let dataset = {
+
+    let cityData = {
         "children": [{"Name":"Московская электронная школа","Count":3,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Пермь</li></ul>"},
             {"Name":"Образовательное телевидение (МособрТВ)","Count":15,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Каспийск</li></ul>"},
             {"Name":"Менторинг в системе образования","Count":3,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Сочи</li></ul>"},
             {"Name":"Субботы московского школьника","Count":3,"City":"Москва","Partners":"<ul><li>Тюмень</li></ul>"},
             {"Name":"Образовательное телевидение (МособрТВ)","Count":2,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Каспийск</li></ul>"},
-            {"Name":"Менторинг в системе образования","Count":3,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Сочи</li></ul>"},
-            {"Name":"Субботы московского школьника","Count":10,"City":"Москва","Partners":"<ul><li>Тюмень</li></ul>"},
+            {"Name":"Менторинг в системе образования","Count":3,"City":"Ижевск","Partners":"<ul><li>Ижевск</li><li>Сочи</li></ul>"},
+            {"Name":"Субботы московского школьника","Count":10,"City":"Сочи","Partners":"<ul><li>Тюмень</li></ul>"},
             {"Name":"Образовательное телевидение (МособрТВ)","Count":2,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Каспийск</li></ul>"},
             {"Name":"Менторинг в системе образования","Count":3,"City":"Москва","Partners":"<ul><li>Ижевск</li><li>Сочи</li></ul>"},
             {"Name":"Субботы московского школьника","Count":3,"City":"Москва","Partners":"<ul><li>Тюмень</li></ul>"},
@@ -70,125 +68,173 @@ $s = array_unique($s);
 
     };
 
-    let diameter = 750;
-    let color = d3.scaleOrdinal(d3.schemeCategory10);
+    makeCityFilter()
 
-    // Three function that change the tooltip when user hover / move / leave a cell
-    let mouseover = function(d) {
-        Tooltip.style("opacity") === "1" ? Tooltip.style("opacity", 0):
+    function makeChart(data, selectedCity){
+        console.log(cityData)
+        console.log(data)
+        let dataset = Object.assign({},data)
+        // Three function that change the tooltip when user hover / move / leave a cell
+        if (document.getElementById("bubble")) {
+            document.getElementById("bubble").remove()
+        }
+
+
+
+        if (selectedCity) {
+            dataset['children'] = dataset['children'].filter(function (i) {
+                return i['City'] === selectedCity;
+            })
+            console.log (dataset)
+        }
+        let mouseover = function(d) {
+            Tooltip.style("opacity") === "1" ? Tooltip.style("opacity", 0):
+                Tooltip
+                    .style("opacity", 1)
+                    .html("<h5>Реализуют проект:<br>" + d.data.Partners + "</h5>")
+                    .style("left", (d.x + d3.mouse(this)[0]+ 30) + "px")
+                    .style("top", (d.y + d3.mouse(this)[1] + 30) + "px")
+                    .style("class", "bubble-tooltip");
+            console.log("mouseover")
+
+        }
+        let mouseup = function (d) {
             Tooltip
-                .style("opacity", 1)
-                .html("<h5>Реализуют проект:<br>" + d.data.Partners + "</h5>")
-                .style("left", (d.x + d3.mouse(this)[0]+ 30) + "px")
-                .style("top", (d.y + d3.mouse(this)[1] + 30) + "px")
-                .style("class", "bubble-tooltip");
-        console.log("mouseover")
+                .style("opacity", 0)
+        }
 
-    }
-    let mouseup = function (d) {
-        Tooltip
+        var mouseleave = function(d) {
+            Tooltip
+            // .style("opacity", 0)
+
+        }
+
+        let diameter = 750;
+        let color = d3.scaleOrdinal(d3.schemeCategory10);
+
+        let bubble = d3.pack(dataset)
+            .size([diameter, diameter])
+            .padding(1.5);
+
+        console.log(bubble)
+
+        let svg = d3.select("#my_dataviz")
+            .append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .attr("class", "bubble")
+            .attr("id", "bubble");
+
+        let nodes = d3.hierarchy(dataset)
+            .sum(function(d) { return d.Count; });
+
+        let node = svg.selectAll("node")
+            .data(bubble(nodes).descendants())
+            .enter()
+            .filter((d)=> !d.children)
+            .append("g")
+            .attr("class", "node")
+            .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")");
+
+
+        node.append("circle")
+            .attr("id", guid())
+            .attr("r", function(d) {
+                return d.r;
+            })
+            .style("fill", function(d,i) {
+                return color(i);
+            })
+            .attr("stroke", "white")
+            .attr("transition", "1500")
+            .style("stroke-width", "2px");
+
+        node.append('foreignObject')
+            .attr("id", guid())
+            .attr('x', function(d) {
+                return - d.r * 0.75;
+            })
+            .attr('y', function(d) {
+                return - d.r * 0.4;
+            })
+            .attr('width', function(d) {
+                return d.r * 1.5;
+            })
+            .attr('height', function(d) {
+                return d.r * 1.5;
+
+            })
+            .on("click", mouseover)
+            .on("mouseleave", mouseleave)
+            .style("cursor", "pointer")
+            .html(function (d,i) {
+                return '<div style="width:' +
+                    d.r * 1.5 +
+                    'px; background-color: ' +
+                    color(i) +
+                    '; ' +
+                    'text-align: center; font-size:' +
+                    d.r/7 +
+                    'px;' +
+                    'line-height: ' +
+                    d.r/7 +
+                    'px"><p style="color: white">' +
+                    d.data.Name +
+                    '<br><br>--' +
+                    d.data.City +
+                    '--</p></div>';
+            });
+
+        d3.select(self.frameElement)
+            .style("height", diameter + "px");
+
+        // create a tooltip
+        let Tooltip = d3.select("#my_dataviz")
+            .append("div")
             .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px");
+
+
+
+
     }
 
-    let mousemove = function(d) {
-        // Tooltip
+    function makeCityFilter(){
+        let uniqCities =  function () {
+            function onlyUnique(value, index, self) {
+                return self.indexOf(value) === index;
+            }
+            let cities = []
+            for (let v in cityData['children']){
+                cities.push(cityData['children'][v]['City'])
+            }
+            let u = cities.filter(onlyUnique)
+            return u
+        };
 
+        (function($){
+            let cityList = $('<select/>',{id:'cityListSelect', onChange:'updateSVG(this)'});
+            $('<option />', {value: 'Все', text: 'Все'}).appendTo(cityList);
+            uniqCities().forEach(function (item) {
+                $('<option />', {value: item, text: item}).appendTo(cityList);
+            })
+            cityList.appendTo('#my_dataviz');
+        })(jQuery);
 
     }
-    var mouseleave = function(d) {
-        Tooltip
-        // .style("opacity", 0)
-
+    function updateSVG(selectedCity){
+        console.log(selectedCity.value)
+        makeChart(cityData, selectedCity.value);
     }
 
-    let bubble = d3.pack(dataset)
-        .size([diameter, diameter])
-        .padding(1.5);
 
-    let svg = d3.select("#my_dataviz")
-        .append("svg")
-        .attr("width", diameter)
-        .attr("height", diameter)
-        .attr("class", "bubble");
+    makeChart(cityData)
 
-    let nodes = d3.hierarchy(dataset)
-        .sum(function(d) { return d.Count; });
 
-    let node = svg.selectAll("node")
-        .data(bubble(nodes).descendants())
-        .enter()
-        .filter((d)=> !d.children)
-        .append("g")
-        .attr("class", "node")
-        .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")");
-
-    node.append("circle")
-        .attr("id", guid())
-        .attr("r", function(d) {
-            return d.r;
-        })
-        .style("fill", function(d,i) {
-            return color(i);
-        })
-        .attr("stroke", "white")
-        .attr("transition", "1500")
-        .style("stroke-width", "2px")
-    // .on("mouseover", mouseover)
-    // .on("mousemove", mousemove)
-    // .on("mouseleave", mouseleave)
-
-    ;
-
-    node.append('foreignObject')
-        .attr("id", guid())
-        .attr('x', function(d) {
-            return - d.r * 0.75;
-        })
-        .attr('y', function(d) {
-            return - d.r * 0.4;
-        })
-        .attr('width', function(d) {
-            return d.r * 1.5;
-        })
-        .attr('height', function(d) {
-            return d.r * 1.5;
-
-        })
-        // .on("mouseover", mouseover)
-        .on("click", mouseover)
-        //.on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
-        .style("cursor", "pointer")
-        .html(function (d,i) {
-            return '<div style="width:' +
-                d.r * 1.5 +
-                'px; background-color: ' +
-                color(i) +
-                '; ' +
-                'text-align: center; font-size:' +
-                d.r/7 +
-                'px;' +
-                'line-height: ' +
-                d.r/7 +
-                'px"><p style="color: white">' +
-                d.data.Name +
-                '<br><br>--' +
-                d.data.City +
-                '--</p></div>';
-        });
-
-    d3.select(self.frameElement)
-        .style("height", diameter + "px");
-
-    // create a tooltip
-    let Tooltip = d3.select("#my_dataviz")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px");
 
 </script>
